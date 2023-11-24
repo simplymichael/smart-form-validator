71c47fc973a4c3d3eb0ddd9b991ed44e6c97316e
simplymichael <simplymichaelorji@gmail.com>
2023-11-24 11:08:20 +0100

    refactor(helpers:array): delete unused (array) helper methods

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,8 @@
 "use strict";
 
-const errorMessages = require("./error-messages");
-
 const APP_CLASSNAME = "sfv";
 const VALID_FIELD_CLASSNAME = "ok";
 const INVALID_FIELD_CLASSNAME = "error";
-
-const array = {
-  iterator: arrayIterator,
-  functionArrayIterator: functionArrayIterator,
-};
 
 const is = {
   array: isArray,
@@ -26,7 +19,6 @@ const object = {
 // eslint-disable-next-line
 module.exports = {
   is,
-  array,
   object,
   normalizeId,
   validateId,
@@ -34,49 +26,6 @@ module.exports = {
   VALID_FIELD_CLASSNAME,
   INVALID_FIELD_CLASSNAME,
 };
-
-function arrayIterator(arr) {
-  if(!isArray(arr)) {
-    throw new TypeError(
-      errorMessages.functionParamExpectsType
-        .replace(":param:", "arr")
-        .replace(":type:", "an array")
-    );
-  }
-
-  let i = 0;
-  let result;
-  const generator = (function *nextGenerator() {
-    while(i < arr.length) {
-      yield(arr[i++]);
-    }
-  }());
-
-  return {
-    advance: () => result = generator.next(),
-    done: () => result.done,
-    key: () => i,
-    reset: () => i = 0,
-    value: () => result.value,
-  };
-}
-
-/**
- * Get an Iterator for an array of functions.
- * @param {Function[]}: The array of functions to iterate, invoking each one on next iteration.
- * @return {Object}: An Iterator object.
- */
-function functionArrayIterator(arr) {
-  if(!isArray(arr) || !(arr.every(fn => typeof fn === "function"))) {
-    throw new TypeError(
-      errorMessages.functionParamExpectsType
-        .replace(":param:", "arr")
-        .replace(":type:", "an array of functions")
-    );
-  }
-  
-  return arrayIterator(arr);
-}
 
 function isArray(data) {
   return Array.isArray(data);
